04d03f92d6d45b12c9e5278b3b117da1d127bca3
JelsonJr <jelson.rodrigues.junior@gmail.com>
2024-09-04 14:14:42 -0300

    adicionando alis

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createMemoryHistory, createRouter } from "vue-router";
 import Home from "@/pages/Home.vue";
 
-const routes = [{ path: "/", component: Home }];
+const routes = [{ path: "/", component: Home, alias: "/home" }];
 
 const router = createRouter({
   history: createMemoryHistory(),
