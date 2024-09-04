25c57962148c59ad245d304fef760781f8335d46
JelsonJr <jelson.rodrigues.junior@gmail.com>
2024-09-04 12:10:35 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="bg-yellow-500 h-screen w-screen">
+    <RouterView></RouterView>
+  </main>
+</template>
---
+++ src/pages/Home.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="bg-yellow-700">
+    <h1>hello world</h1>
+  </div>
+</template>
